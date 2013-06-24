5ab69cad6e41fa67cbd68bdb2b3902eb74ecc924
leticiamara <leticiamara.fernandes@gmail.com>
2013-06-24 16:04:27 -0300

    Adicionado o web server

---
+++ AssistiWebApp/src/web_server/Server.java
@@ -0,0 +1,13 @@
+package web_server;
+
+public class Server {
+	
+	private static final String apiKey = "95ba8e4e85f5add6b0de44a9e213ef31";
+
+	public static String getApikey() {
+		return apiKey;
+	}
+	
+	
+
+}
