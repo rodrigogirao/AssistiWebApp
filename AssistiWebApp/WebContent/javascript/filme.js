861479f4d62e7918785ab66596ef2dcee56d8f5a
leticiamara <leticiamara.fernandes@gmail.com>
2013-07-11 00:46:23 -0300

    Feito a descrição de um filme clicado.

--- AssistiWebApp/WebContent/javascript/filme.js
+++ AssistiWebApp/WebContent/javascript/filme.js
@@ -1,10 +1,12 @@
 $(function(){
 	
 	var server = "http://localhost" + ":8080/AssistiWebApp/servico/feed";
+	var descricaoFilme = "http://localhost" + ":8080/AssistiWebApp/servico/feed/filme/";
 	
 	function addFilmeAoHtml(id, titulo, dataLancamento, imagem){
-		var $filme = "<li id="+id+">" +
-						"<a href=''>" +
+		var $filme = "<li>" +
+						"<a href='#descricaoFilme' id='"+id+"'>" +
+						"<div class='idFilme'>" + id + "</div>" +
 							"<img src='http://d3gtl9l2a4fn1j.cloudfront.net/t/p/w92"+imagem+"'>"+
 							"<h2>"+titulo+"</h2>"+
 							"<p>Lancamento "+dataLancamento+"</p>"+
@@ -13,8 +15,24 @@ $(function(){
 		
 		$("#feedDeFilmes").append($filme);
 		$("#feedDeFilmes").listview( "refresh" );
+		$("#"+id).on("click",onFilmeClick);
+		$(".idFilme").hide();
 	}
 	
+	function addDescricaoFilmeAoHtml(id, titulo, descricao, link, lancamento, popularidade){
+		var $filmeDescricao = "<li data-role='list-divider'>"+titulo+"</li>" +
+									"<li id="+id+">" +
+									"<a href=''>" +
+									"<h2>"+titulo+"</h2>"+
+									"<p><strong> Site do Filme "+link+"</strong></p>"+
+									"<p>Descricao "+descricao+"</p>"+
+									"<p> Lancamento"+lancamento+"</p>"+
+									"<p class='ui-li-aside'><strong> Popularidade </strong>"+popularidade+"</p>"+
+									"</a>" +
+									"</li>";
+		$("#descricao").append($filmeDescricao);
+		$("#descricao").listview("refresh");
+	}
 	
 	
 	function listarFilmesDoFeed(){
@@ -33,6 +51,32 @@ $(function(){
 		
 	}
 	
+	function listarDescricaoFilme(id) {
+		$.getJSON(descricaoFilme+id).done(
+			function(resultado){
+				
+				console.log("resultado 2: " ,resultado);
+				$("#descricao").empty();
+					
+					addDescricaoFilmeAoHtml(resultado.id, resultado.original_title,
+							resultado.overview, resultado.homepage,
+							resultado.release_date, resultado.popularity);
+				
+			});
+		
+	}
+	
+	function onFilmeClick(){
+		$lastClicked = $(this);
+		console.log("ENTROU NO Onfilmeclick");
+		var id = $lastClicked.children(".idFilme").text();
+		console.log("Ultimo clicado",$lastClicked.text());
+		console.log("ENTROU NO Onfilmeclick e imprimiu id", id);
+		listarDescricaoFilme(id);
+		
+	}
+	
 	$("#feed").click(listarFilmesDoFeed);
 	listarFilmesDoFeed();
+	
 });
--- AssistiWebApp/src/webServer/WSFeed.java
+++ AssistiWebApp/src/webServer/WSFeed.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import util.ApiKey;
@@ -14,6 +15,7 @@ import util.RequisicaoHTTP;
 public class WSFeed {
 	
 	private static final String FILMES_POPULARES = "http://api.themoviedb.org/3/movie/popular";
+	private static final String DESCRICAO_FILME = "http://api.themoviedb.org/3/movie/";
 	
 	@GET
 	@Produces("application/json")
@@ -22,6 +24,19 @@ public class WSFeed {
         String retornoJson = http.getUrl(FILMES_POPULARES, ApiKey.getApikey());
  
         return retornoJson;
+	}
+	
+	@GET
+	@Path("/filme/{idFilme}")
+	@Produces("application/json")
+	public String retornarFilmePorId(@PathParam("idFilme") String idFilme) throws IOException{
+		
+		RequisicaoHTTP http = new RequisicaoHTTP();
+		String retornoJson = http.getUrl(DESCRICAO_FILME+idFilme, ApiKey.getApikey());
+		
+		System.out.println(retornoJson);
+		
+		return retornoJson;
 	}
 
 }
